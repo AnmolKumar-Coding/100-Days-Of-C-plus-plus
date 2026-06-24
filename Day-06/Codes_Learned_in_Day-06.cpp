4199b4f2ae38165b0599988f585aa2780e9ab501
Anmol Kumar <277338667+AnmolKumar-Coding@users.noreply.github.com>
2026-06-24 06:21:25 +0530

    Uploaded my codes using VS code not like copy and paste i was doing before.

--- Day-06/Codes_Learned_in_Day-06.cpp
+++ Day-06/Codes_Learned_in_Day-06.cpp
@@ -23,81 +23,75 @@ int main(){
         S = S + 2;
         
     }
-}
 
 
-// Find sum from 1 to N?
 
-int main(){
+// Find sum from 1 to N1?
 
-    int N;
+    int N1;
 
     int sum = 0;
     int num = 1;
 
-    cout << " Type the value of N" << endl;
-    cin >> N;
+    cout << " Type the value of N1" << endl;
+    cin >> N1;
 
-    while (num <= N){
+    while (num <= N1){
         
         sum = sum + num;
 
         num = num + 1;
 
     } 
-    cout << " the num of 1 to " << N << endl;
+    cout << " the num of 1 to " << N1 << endl;
     cout << sum << endl;
-}
+
 
 
 //find n! ?
 
-int main(){
-
-    double N;
+    double n;
 
     double ans = 1;
-    double num = 1;
+    double num1 = 1;
 
-    cout << "enter the value of N" << endl;
-    cin >> N;
+    cout << "enter the value of n" << endl;
+    cin >> n;
 
-    while(num <= N){
+    while(num1 <= n){
 
-        ans = ans * num;
+        ans = ans * num1;
 
-        num = num + 1;
+        num1 = num1 + 1;
     }
 
-    cout << "Multiplication of 1 to " << N << " is " << endl;
+    cout << "Multiplication of 1 to " << n << " is " << endl;
 
     cout << ans << endl;
-}
+
 
 
 // find all 1 to n is a prime numbers.
-
-int main(){
     
-    int n;
+    int n1;
     int s = 2;
 
     cout << "enter the value of n :-" << endl;
-    cin >> n;
+    cin >> n1;
 
-    cout << "the prime numbers between 0 and " << n << " is :-" <<endl;
+    cout << "the prime numbers between 0 and " << n1 << " is :-" <<endl;
 
-    if (n>=2){
+    if (n1>=2){
         cout << "2 is prime" << endl;
     }
-    if (n>=3){
+    if (n1>=3){
         cout << "3 is prime" << endl;
         }
-    if (n >= 5){
+    if (n1 >= 5){
         cout << "5 is prime" << endl;
     }
 
-    while (n>=s){
+    while (n1>=s){
         if((s%2 != 0) &&(s%3 != 0) && (s%5 != 0) ){
                 cout << s << " is prime" << endl;
         }
