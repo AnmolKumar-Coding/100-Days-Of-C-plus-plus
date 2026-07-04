80e1b7a5d1dc05db1bc9f8223ff0354ae1ec260c
Anmol Kumar <277338667+AnmolKumar-Coding@users.noreply.github.com>
2026-07-04 23:28:55 +0530

    Learned How to covert small Decimals into Binary.

--- Day-15/Decimal_To_Binary.cpp
+++ Day-15/Decimal_To_Binary.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
 
+int main(){
+
+    int n;
+
+    cout << "Enter the value of n :- " << endl;
+    cin >> n;
+
+    cout << "The binary form of" << " " << n << " is :-" << endl;
+
+    int i = 0;
+
+    int ans = 0;
+
+    while(n>0){
+        
+        int bit = n & 1;
+
+        ans = ans + ( bit * pow(10, i));
+
+        n = n >> 1;
+        i++;
+    }
+
+    cout << ans << endl;
+}
