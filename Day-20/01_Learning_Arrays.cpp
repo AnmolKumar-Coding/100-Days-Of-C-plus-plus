d91123a9d7902a0f7e61e1c53a9ea9dd30330ca7
AnmolKumar-Coding <277338667+AnmolKumar-Coding@users.noreply.github.com>
2026-07-12 23:59:46 +0530

    learned basic arrays.... i was not uploading in git beacuse i was making the roadmap for the college which used 2 days with ai and also connected my git with codex... and the day before commit was also corrected by ai which helped me find error... bit dont worry from now onwards i will upload daily and if a day has been skipped a valid and truthful reason will be provided.

---
+++ Day-20/01_Learning_Arrays.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int first[10];                       // this is the declarion of arrays.
+    cout << first[0] << endl << endl;    // since no value is stored in the array so random values;
+
+    int second[3]{1,5,4};               //    //we initialised the array 
+
+    // To access the FIRST value (Index 0)
+    cout << "Value at 0 index :-  " <<  second[0] << endl ;        // accesing an element inside the array
+    cout << "Value at 1 index :-  " << second[1] << endl ;         // values inside an arrays is stored in Index. and the first value is stored in index 0, and last value is stores in the (full size of array - 1), and to access that value we write the array name in int array_name [here the index in which the value is stored.]
+
+    // To access the last value (Index 2)
+    cout << "Value at 2 index :- " << second[2] << endl ;
+
+    int third [100]{0};                 // initialsing all values of arrays as 0
+    cout << "Value at 67 index :- " << third[67] << endl;
+
+
+    //initialising all locations with 1 [not possible with below line]
+    int fourth[10] = {1};
+    cout << "Value at 0 index :- " << fourth[0] << endl;
+    cout << "Value at 2 index :- " << fourth[3] << endl;
+
+}
